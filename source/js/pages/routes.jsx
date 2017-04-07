18924b5c4f353eee423f439c92daec6e93b34b1a
Alexander Katsero <alexanderkatsero@gmail.com>
2017-04-07 19:12:30 +0300

    minor changes

--- source/js/pages/routes.jsx
+++ source/js/pages/routes.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from './Home'
-import About from './About'
 
 import NotFound from './NotFound'
 
@@ -10,7 +9,6 @@ const Routes = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
-      <Route path='/about' component={About} />
 
       <Route path='*' component={NotFound} />
     </Switch>

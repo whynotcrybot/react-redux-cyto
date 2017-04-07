871132b8633c8e8dbe22107b08f97664e47744e2
Alexander Katsero <alexanderkatsero@gmail.com>
2017-04-07 17:51:50 +0300

    render sample graph

---
+++ source/js/ducks/cytoscape.js
@@ -0,0 +1,70 @@
+const ADD_CLUSTER = 'cyto/ADD_CLUSTER'
+const ADD_NODE = 'cyto/ADD_NODE'
+const ADD_EDGE = 'cyto/ADD_EDGE'
+
+export function addCluster () {
+  return {
+    type: ADD_CLUSTER
+  }
+}
+
+export function addNode (id) {
+  return {
+    type: ADD_NODE,
+    id: id
+  }
+}
+
+export function addEdge (source, target) {
+  return {
+    type: ADD_EDGE,
+    source: source,
+    target: target
+  }
+}
+
+const INITIAL_STATE = {
+  cluster: {
+    nodes: [
+      { data: { id: 'a', foo: 3, bar: 5, baz: 7 } },
+      { data: { id: 'b', foo: 7, bar: 1, baz: 3 } },
+      { data: { id: 'c', foo: 2, bar: 7, baz: 6 } },
+      { data: { id: 'd', foo: 9, bar: 5, baz: 2 } },
+      { data: { id: 'e', foo: 2, bar: 4, baz: 5 } }
+    ],
+
+    edges: [
+      { data: { id: 'ae', weight: 1, source: 'a', target: 'e' } },
+      { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
+      { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
+      { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
+      { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
+      { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
+      { data: { id: 'de', weight: 7, source: 'd', target: 'e' } }
+    ]
+  },
+  nodes: [],
+  edges: [],
+  layout: {
+    name: 'breadthfirst',
+    directed: true,
+    padding: 10
+  }
+}
+
+export default function counterReducer (state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case ADD_NODE:
+      return {...state, nodes: state.nodes.push({data: {id: action.id}})}
+    case ADD_EDGE:
+      return {...state, edges: state.edges.push({ data: { source: action.source, target: action.target } })}
+    case ADD_CLUSTER:
+      return {
+        ...state,
+        nodes: [...state.nodes, ...state.cluster.nodes],
+        edges: [...state.edges, ...state.cluster.edges]
+      }
+    default:
+      return state
+  }
+}
--- source/js/pages/Home/index.jsx
+++ source/js/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import cytoscape from 'cytoscape'
+import { addNode, addEdge, addCluster } from 'ducks/cytoscape'
 
 import * as styles from './styles.css'
 
@@ -10,68 +12,63 @@ class Home extends React.Component {
   }
 
   renderCytoscapeElement () {
-    console.log('* Cytoscape.js is rendering the graph..')
-
     this.cy = cytoscape({
       container: document.getElementById('cy'),
 
-      boxSelectionEnabled: false,
-      autounselectify: true,
+      boxSelectionEnabled: true,
+      autounselectify: false,
+      zoomingEnabled: false,
+      zoom: 1.2,
 
       style: cytoscape.stylesheet()
         .selector('node')
         .css({
-          'height': 80,
-          'width': 80,
-          'background-fit': 'cover',
-          'border-color': '#000',
-          'border-width': 3,
-          'border-opacity': 0.5,
-          'content': 'data(name)',
-          'text-valign': 'center'
+          'height': 20,
+          'width': 20,
+          'font-size': '12px',
+          'text-valign': 'center',
+          'text-halign': 'center',
+          'background-color': '#555',
+          'text-outline-color': '#555',
+          'text-outline-width': '2px',
+          'color': '#fff',
+          'content': 'data(id)',
+          'overlay-padding': '6px',
+          'z-index': 10
         })
         .selector('edge')
         .css({
-          'width': 6,
-          'target-arrow-shape': 'triangle',
-          'line-color': '#ffaaaa',
-          'target-arrow-color': '#ffaaaa',
-          'curve-style': 'bezier'
-        }),
-      elements: {
-        nodes: [
-          { data: { id: 'cat' } },
-          { data: { id: 'bird' } },
-          { data: { id: 'ladybug' } },
-          { data: { id: 'aphid' } },
-          { data: { id: 'rose' } },
-          { data: { id: 'grasshopper' } },
-          { data: { id: 'plant' } },
-          { data: { id: 'wheat' } }
-        ],
-        edges: [
-          { data: { source: 'cat', target: 'bird' } },
-          { data: { source: 'bird', target: 'ladybug' } },
-          { data: { source: 'bird', target: 'grasshopper' } },
-          { data: { source: 'grasshopper', target: 'plant' } },
-          { data: { source: 'grasshopper', target: 'wheat' } },
-          { data: { source: 'ladybug', target: 'aphid' } },
-          { data: { source: 'aphid', target: 'rose' } }
-        ]
-      },
-      layout: {
-        name: 'breadthfirst',
-        directed: true,
-        padding: 10
-      }
+          'width': 2,
+          'opacity': 0.4,
+          'line-color': '#bbb',
+          'overlay-padding': '3px'
+        })
     })
+  }
+
+  renderElements (nodes, edges) {
+    nodes.forEach(node => this.cy.add(node))
+    edges.forEach(edge => this.cy.add(edge))
+  }
+
+  renderLayout (layout) {
+    this.cy.layout(layout)
+  }
+
+  componentWillMount () {
+    if (!this.props.nodes.length) this.props.addCluster()
+  }
+
+  componentWillReceiveProps (props) {
+    this.renderElements(props.nodes, props.edges)
+    this.renderLayout(props.layout)
   }
 
   componentDidMount () {
     this.renderCytoscapeElement()
   }
 
-  render () {
+  render (props) {
     return (
       <div>
         <div className={styles.cytoscapeWrapper} id='cy' />
@@ -80,4 +77,19 @@ class Home extends React.Component {
   }
 }
 
-export default Home
+export default connect(
+  function (state) {
+    return {
+      nodes: state.cytoscape.nodes,
+      edges: state.cytoscape.edges,
+      layout: state.cytoscape.layout
+    }
+  },
+  function (dispatch) {
+    return {
+      addNode: (id) => dispatch(addNode(id)),
+      addEdge: (source, target) => dispatch(addEdge(source, target)),
+      addCluster: () => dispatch(addCluster())
+    }
+  }
+)(Home)

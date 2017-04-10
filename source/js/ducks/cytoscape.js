2a620cedfc7f7d20d701ad2c52aeb0be4c38631d
Alexander Katsero <alexanderkatsero@gmail.com>
2017-04-10 12:55:54 +0300

    redux data optimizations

--- source/js/ducks/cytoscape.js
+++ source/js/ducks/cytoscape.js
@@ -1,10 +1,21 @@
-const ADD_CLUSTER = 'cyto/ADD_CLUSTER'
 const ADD_NODE = 'cyto/ADD_NODE'
 const ADD_EDGE = 'cyto/ADD_EDGE'
 
 export function addCluster () {
-  return {
-    type: ADD_CLUSTER
+  return (dispatch, getState) => {
+    const currentNodes = getState().cytoscape.nodes.length
+    const cluster = {
+      nodes: getState().cytoscape.cluster.nodes,
+      edges: getState().cytoscape.cluster.edges
+    }
+
+    for (let i = currentNodes; i < currentNodes + cluster.nodes; i++) {
+      dispatch(addNode(i + 1))
+    }
+
+    cluster.edges.forEach(edge => {
+      dispatch(addEdge(currentNodes + edge.s, currentNodes + edge.t))
+    })
   }
 }
 
@@ -18,29 +29,23 @@ export function addNode (id) {
 export function addEdge (source, target) {
   return {
     type: ADD_EDGE,
-    source: source,
-    target: target
+    source,
+    target
   }
 }
 
 const INITIAL_STATE = {
   cluster: {
-    nodes: [
-      { data: { id: 'a', foo: 3, bar: 5, baz: 7 } },
-      { data: { id: 'b', foo: 7, bar: 1, baz: 3 } },
-      { data: { id: 'c', foo: 2, bar: 7, baz: 6 } },
-      { data: { id: 'd', foo: 9, bar: 5, baz: 2 } },
-      { data: { id: 'e', foo: 2, bar: 4, baz: 5 } }
-    ],
+    nodes: 5,
 
     edges: [
-      { data: { id: 'ae', weight: 1, source: 'a', target: 'e' } },
-      { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
-      { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
-      { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
-      { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
-      { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
-      { data: { id: 'de', weight: 7, source: 'd', target: 'e' } }
+      { s: 1, t: 5 },
+      { s: 1, t: 2 },
+      { s: 2, t: 5 },
+      { s: 2, t: 3 },
+      { s: 3, t: 5 },
+      { s: 3, t: 4 },
+      { s: 4, t: 5 }
     ]
   },
   nodes: [],
@@ -55,14 +60,18 @@ const INITIAL_STATE = {
 export default function counterReducer (state = INITIAL_STATE, action) {
   switch (action.type) {
     case ADD_NODE:
-      return {...state, nodes: [...state.nodes, {data: {id: action.id}}]}
+      return {...state, nodes: [...state.nodes, { data: { id: action.id } }]}
     case ADD_EDGE:
-      return {...state, edges: state.edges.push({ data: { source: action.source, target: action.target } })}
-    case ADD_CLUSTER:
       return {
         ...state,
-        nodes: [...state.nodes, ...state.cluster.nodes],
-        edges: [...state.edges, ...state.cluster.edges]
+        edges: [
+          ...state.edges,
+          { data: {
+            id: action.source + '-' + action.target,
+            source: action.source,
+            target: action.target
+          } }
+        ]
       }
     default:
       return state
